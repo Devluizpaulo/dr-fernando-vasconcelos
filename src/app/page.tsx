74ed4bbe291879aa1b5b5087aedb97ad938fc64a
unknown <luiz14paulo@hotmail.com>
2025-01-14 04:18:03 -0300

    Atualizações no layout e texto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { FaClipboardList, FaHandshake, FaBullhorn, FaClock, FaBuilding, FaExclamationTriangle, FaWhatsapp } from 'react-icons/fa';
+import { FaClipboardList, FaHandshake, FaBullhorn, FaClock, FaBuilding, FaExclamationTriangle, FaWhatsapp, FaBriefcase } from 'react-icons/fa';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import Card from '@/components/Card';
@@ -15,17 +15,18 @@ const Home: FC = () => {
         id="Home"
         className="relative text-white text-center py-10 bg-gradient-to-b from-blue-900 to-blue-600 h-[70vh] flex flex-col justify-center items-center"
       >
+        {/* Imagem de fundo */}
         <div
           className="absolute inset-0 bg-cover bg-center backdrop-blur-3xl transition-all duration-500"
           style={{
-            backgroundImage: 'url(/hero-image.jpg)',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            filter: 'brightness(0.5)'
+            backgroundImage: "url(/hero-image.jpg)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            filter: "brightness(0.5)",
           }}
         ></div>
 
-        {/* Conteúdo do hero */}
+        {/* Conteúdo principal do hero */}
         <div className="relative z-10 max-w-4xl mx-auto text-center px-6">
           <h2 className="text-4xl sm:text-5xl font-semibold mb-6 animate__animated animate__fadeIn animate__delay-1s">
             Direito Trabalhista
@@ -42,6 +43,18 @@ const Home: FC = () => {
             Agende uma Consulta
           </a>
         </div>
+
+        <div className="absolute bg-white opacity-80 rounded-md p-2 shadow-lg top-2 right-4 z-40 sm:hidden">
+          <Image
+            src="/VASCONCELLOS.png" // Verifique o caminho e nome
+            alt="Logo"
+            width={120} // Aumente se necessário
+            height={140}
+            className="" // Ajuste se precisar de um formato diferente
+          />
+        </div>
+
+
       </section>
 
       <section id="services" className="py-20 px-4 bg-gray-100">
@@ -97,6 +110,90 @@ const Home: FC = () => {
           />
         </div>
       </section>
+      <section id="OutrasAreas" className="py-20 px-6 bg-gray-100">
+        <h3 className="text-4xl text-center mb-12 font-bold text-blue-800">
+          Soluções Jurídicas Personalizadas
+        </h3>
+        <p className="text-lg sm:text-xl text-center mb-12 text-gray-600">
+          Mais do que especialistas em Direito Trabalhista, nossa equipe está pronta para oferecer suporte completo em diversas áreas. Veja como podemos ajudá-lo a resolver seus desafios jurídicos com eficiência e estratégia.
+        </p>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+          {[
+            {
+              imageSrc: "/direito-penal.jpg",
+              title: "Direito Penal",
+              description: "Seu aliado nos momentos mais desafiadores. Atuamos com ética e compromisso em sua defesa, garantindo proteção legal em processos criminais.",
+              bgColor: "bg-blue-600",
+              hoverBg: "hover:bg-blue-700",
+              href: "https://wa.me/5511994825120",
+            },
+            {
+              imageSrc: "/direito-tributario.jpg",
+              title: "Direito Tributário",
+              description: "Reduza custos e elimine riscos fiscais. Nossa consultoria oferece soluções inteligentes para garantir compliance e maximizar sua economia.",
+              bgColor: "bg-green-600",
+              hoverBg: "hover:bg-green-700",
+              href: "https://wa.me/5511994825120",
+            },
+            {
+              imageSrc: "/direito-civil.jpg",
+              title: "Direito Cível",
+              description: "De contratos a indenizações, resolvemos suas disputas com rapidez e eficiência, sempre priorizando os melhores resultados para você.",
+              bgColor: "bg-purple-600",
+              hoverBg: "hover:bg-purple-700",
+              href: "https://wa.me/5511994825120",
+            },
+            {
+              imageSrc: "/direito-familia.jpg",
+              title: "Direito de Família",
+              description: "Cuidamos do que mais importa: sua família. Com sensibilidade e expertise, tratamos de questões como divórcios, guarda e pensão alimentícia.",
+              bgColor: "bg-red-600",
+              hoverBg: "hover:bg-red-700",
+              href: "https://wa.me/5511994825120",
+            },
+            {
+              imageSrc: "/direito-consumidor.jpg",
+              title: "Direito do Consumidor",
+              description: "Você não está sozinho. Protegemos seus direitos contra práticas abusivas, cobranças indevidas e contratos irregulares.",
+              bgColor: "bg-orange-600",
+              hoverBg: "hover:bg-orange-700",
+              href: "https://wa.me/5511994825120",
+            },
+            {
+              imageSrc: "/direito-empresarial.jpg",
+              title: "Direito Empresarial",
+              description: "Cresça com segurança. Nossa equipe garante a proteção jurídica da sua empresa, oferecendo suporte estratégico em todas as etapas do negócio.",
+              bgColor: "bg-teal-600",
+              hoverBg: "hover:bg-teal-700",
+              href: "https://wa.me/5511994825120",
+            },
+          ].map((area, index) => (
+            <div
+              key={index}
+              className="bg-white rounded-xl shadow-lg p-8 flex flex-col sm:flex-row items-center justify-between transform transition-transform hover:scale-105 hover:shadow-2xl"
+            >
+              <div className="flex-shrink-0 mb-6 sm:mb-0 sm:w-1/3">
+                <img
+                  src={area.imageSrc}
+                  alt={area.title}
+                  className="w-full h-auto rounded-md object-cover"
+                />
+              </div>
+              <div className="sm:w-2/3 sm:pl-6">
+                <h4 className="text-xl font-bold mb-4 text-gray-800">{area.title}</h4>
+                <p className="text-gray-600 mb-6">{area.description}</p>
+                <a
+                  href={area.href}
+                  className={`inline-block py-2 px-6 rounded-lg text-white font-bold transition-colors ${area.bgColor} ${area.hoverBg}`}
+                >
+                  Fale com um especialista
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
 
       <section id="Sobre" className="bg-white py-20">
         <h3 className="text-4xl text-center mb-12 font-semibold text-blue-800">
@@ -108,19 +205,22 @@ const Home: FC = () => {
             <Image
               src="/adv.png"
               alt="Dr. Fernando Vasconcellos"
-              width={200}
-              height={200}
-              className="w-full h-auto rounded-full"
+              width={100}
+              height={100}
+              className="w-full h-auto rounded-2xl"
             />
           </div>
-
-          {/* Seção de Texto */}
           <div className="lg:w-2/3 text-justify lg:text-left px-8">
             <p className="text-lg sm:text-xl text-gray-700 mb-8 leading-relaxed">
-              Nosso escritório foi fundado com o objetivo de fornecer um atendimento jurídico personalizado e de excelência. Com mais de 15 anos de experiência, o escritório se destaca pela ética, transparência e dedicação incansável aos seus clientes, conquistando uma sólida reputação na áreas de Direito Trabalhista nas mais diversas situações.
+              Nosso escritório foi fundado com o objetivo de fornecer um atendimento jurídico personalizado e de excelência.
+              Com foco em ética, transparência e dedicação, nos destacamos na área de Direito Trabalhista, lidando com as mais
+              diversas situações e conquistando uma sólida reputação entre nossos clientes.
             </p>
-            <p className="text-lg sm:text-xl text-gray-600 leading-relaxed">
-              Nossa missão é garantir que os direitos de nossos clientes sejam protegidos com eficiência e assertividade, oferecendo soluções personalizadas, seja através de acordos ou ações judiciais. Trabalhamos de forma estratégica e próxima, assegurando que cada caso receba atenção dedicada para alcançar o melhor resultado, sempre com o compromisso de buscar a justiça para nossos clientes.
+            <p className="text-lg sm:text-xl text-gray-600 mb-8 leading-relaxed">
+              Nossa missão é garantir que os direitos de nossos clientes sejam protegidos com eficiência e assertividade.
+              Oferecemos soluções personalizadas, seja por meio de acordos ou ações judiciais, trabalhando sempre de forma estratégica
+              e próxima ao cliente. Nos comprometemos a dedicar atenção exclusiva a cada caso, com o objetivo de alcançar o melhor resultado
+              e garantir que a justiça seja feita de maneira plena e eficaz.
             </p>
           </div>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,75 +3,56 @@ import Image from 'next/image';
 
 const Header = () => {
   return (
-    <header className="bg-white py-6 shadow-xl">
+    <header className="bg-white py-1 shadow-xl">
       <div className="max-w-full mx-auto px-6 flex flex-col sm:flex-row justify-between items-center">
         {/* Contêiner do logo e texto */}
-        <div className="flex flex-col sm:flex-row items-center space-x-6 p-3 bg-white rounded-xl shadow-xl backdrop-blur-md">
-          {/* Logo */}
+        <div className="sm:block hidden">
           <Image
-            src="/VASCONCELLOS.jpg" // Substitua pelo caminho da sua imagem de logo
-            alt="Logo"
-            width={140} // Tamanho fixo do logo
-            height={140} // Tamanho fixo do logo
-            className="rounded-full w-36 h-32 sm:w-24 sm:h-34 md:w-30 md:h-30" // Responsividade no tamanho do logo
+            src="/VASCONCELLOS.png" // Substitua pelo caminho correto da sua imagem
+            alt="Logo da Vasconcellos Advocacia"
+            width={280}
+            height={280}
+            className="px-1 py-2 shadow-md"
+            priority // Otimiza o carregamento do logo
           />
-          {/* Título e subtítulo */}
-          <div className="text-blue-950" style={{ fontFamily: 'Canto Brush' }}>
-            <h1 className="text-4xl font-bold tracking-tight leading-tight sm:text-3xl md:text-2xl">
-              FERNANDO <br /> VASCONCELLOS
-            </h1>
-            <p className="text-sm text-right font-serif tracking-wide mt-1 opacity-75 sm:text-xs md:text-xs">
-              ADVOGADO
-            </p>
-          </div>
         </div>
 
-        {/* Ícones de redes sociais */}
-        <div className="flex space-x-6 mt-4 sm:mt-0">
-          <Link href="https://www.instagram.com" target="_blank" aria-label="Instagram">
-            <div className="bg-white p-3 rounded-full shadow-lg hover:scale-110 hover:shadow-2xl hover:shadow-[#E4405F] transition-all duration-300 ease-in-out">
-              <Image
-                src="/insta.png"
-                alt="Instagram"
-                width={42}
-                height={42}
-                className="w-8 h-8 text-[#E4405F]"
-              />
-            </div>
-          </Link>
-
-          <Link href="https://www.facebook.com" target="_blank" aria-label="Facebook">
-            <div className="bg-white p-3 rounded-full shadow-lg hover:scale-110 hover:shadow-2xl hover:shadow-[#1877F2] transition-all duration-300 ease-in-out">
-              <Image
-                src="/Facebook.png"
-                alt="Facebook"
-                width={42}
-                height={42}
-                className="w-8 h-8 text-[#1877F2]"
-              />
-            </div>
-          </Link>
-
-          <Link href="https://x.com" target="_blank" aria-label="X (Twitter)">
-            <div className="bg-white p-3 rounded-full shadow-lg hover:scale-110 hover:shadow-2xl hover:shadow-[#1DA1F2] transition-all duration-300 ease-in-out">
-              <Image
-                src="/x.png"
-                alt="Twitter"
-                width={42}
-                height={42}
-                className="w-8 h-8 text-[#1DA1F2]"
-              />
-            </div>
-          </Link>
-        </div>
-
-        {/* Menu Mobile */}
-        <div className="md:hidden">
-          <button className="text-white">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-          </button>
+        <div className="flex space-x-4 mt-4 sm:mt-0">
+          {[{
+            href: "https://www.instagram.com",
+            src: "/insta.png",
+            alt: "Instagram",
+            color: "#E4405F",
+          }, {
+            href: "https://www.facebook.com",
+            src: "/Facebook.png",
+            alt: "Facebook",
+            color: "#1877F2",
+          }, {
+            href: "https://x.com",
+            src: "/x.png",
+            alt: "Twitter",
+            color: "#1DA1F2",
+          }].map((icon, index) => (
+            <Link
+              key={index}
+              href={icon.href}
+              target="_blank"
+              aria-label={`Link para ${icon.alt}`}
+            >
+              <div
+                className={`bg-white p-3 rounded-full shadow-lg hover:scale-110 hover:shadow-xl hover:shadow-[${icon.color}] transition-all duration-300 ease-in-out`}
+              >
+                <Image
+                  src={icon.src}
+                  alt={icon.alt}
+                  width={42}
+                  height={42}
+                  className="w-8 h-8"
+                />
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </header>
